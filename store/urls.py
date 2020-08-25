e3d5cf0c3c4688591782f01ec8fb35d8c6a5acce
Frank <kipfrankline@gmail.com>
2020-08-25 22:04:16 +0300

    Add authentication function: register, login and logout

--- store/urls.py
+++ store/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('accounts/register/', views.register, name='register'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
 ]
