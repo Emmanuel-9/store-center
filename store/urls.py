010061c937328a7d90abc534b2cdc09a4f0d5e8c
Frank <kipfrankline@gmail.com>
2020-08-25 21:43:30 +0300

    Add authentication function: register, login and logout

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path,include
 from django.conf.urls import url
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    path('',views.home,name='home'),
+    path('accounts/register/', views.register, name='register'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+
 ]
 
--- store/views.py
+++ store/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+
+login_required(login_url='/accounts/login/')
+def home(request):
+
+    return render(request, 'index.html', locals())
+
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
             form.save()
-        return redirect("index")
+        return redirect("login")
     else:
         form = RegisterForm()
     return render(response,'registration/register.html',{'form':form})
