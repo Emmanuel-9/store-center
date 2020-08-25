3ec293d7139cc86a2b0a42e273290fefabf10262
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-25 17:08:08 +0300

    Add the rest_framework to the installed apps.

--- storecenter/settings.py
+++ storecenter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'crispy_forms',
     'bootstrap4',
     'store.apps.StoreConfig',
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'storecenter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store_center',
+        'USER': 'manuel',
+        'PASSWORD': '.Sct.111',
     }
 }
 
@@ -108,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
