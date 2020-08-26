01a6bfd2b6699ef1a9c5328a85699c813d9d1c3e
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-26 20:34:04 +0300

    Update spacing of cards on homepage
    update links on navbar
    
    Update user profile

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -29,9 +31,16 @@ class UserProfile(models.Model):
     email = models.EmailField()
 
     def __str__(self):
-        return f'{self.user.username} '
+        return f'{self.user.username}'
 
-    @classmethod
-    def get_all_userprofiles(cls):
-        return cls.objects.all()
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
+
+    
     
