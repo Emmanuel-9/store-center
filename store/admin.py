719a335d663dd8fc1d43069e3d33a36e7ab854b3
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-27 15:33:12 +0300

    update homepage

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Goods,StorageUnits,UserProfile,Category
+from .models import Goods,StorageUnits,UserProfile,Category,Slot
 
 # Register your models here.
 admin.site.register(Goods)
 admin.site.register(StorageUnits)
 admin.site.register(UserProfile)
 admin.site.register(Category)
+admin.site.register(Slot)
