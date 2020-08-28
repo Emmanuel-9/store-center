4f428cb457c24e6544be0a2564d64208f683f4bf
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-28 09:35:49 +0300

    Add the test classes.

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CategoryTest(TestCase):
+    def setUp(self):
+        pass
+
+
+    def test_instance(self):
+        pass
+
+    def test_saving_category(self):
+        pass
+
+    def test_deleting_category(self):
+        pass
+
+class SlotTest(TestCase):
+    def setUp(self):
+        pass    
